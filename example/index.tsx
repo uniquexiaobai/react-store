8cb0cfe01a2903a151d32347a890fb2dfa529bbf
uniquexiaobai <uniquexiaobai@gmail.com>
2020-08-02 00:49:16 +0800

    feat: support middlewares

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import {
-  Hello,
-  Provider,
-  configureStore,
-  useSelector,
-  useDispatch,
-} from '../.';
+import { Provider, configureStore, useSelector, useDispatch } from '../.';
 
 const { useEffect } = React;
 
@@ -23,12 +17,23 @@ const Count = () => {
 const Button = () => {
   const dispatch = useDispatch();
   const inc = () => dispatch({ type: 'inc' });
+  const incAsync = () =>
+    dispatch(dispatch => {
+      setTimeout(() => {
+        dispatch({ type: 'inc' });
+      }, 2000);
+    });
 
   useEffect(() => {
     console.log('Button render');
   });
 
-  return <button onClick={inc}>+</button>;
+  return (
+    <div>
+      <button onClick={inc}>inc</button>
+      <button onClick={incAsync}>inc async</button>
+    </div>
+  );
 };
 
 const Test = () => {
@@ -40,19 +45,41 @@ const Test = () => {
 };
 
 const App = () => {
-  const reducer = (action, state) => ({ ...state, num: state.num + 1 });
-  const preloadedState = { num: 1 };
-  const store = configureStore({ reducer, preloadedState });
-
   return (
-    <Provider store={store}>
-      <div className='App'>
-        <Count />
-        <Button />
-        <Test />
-      </div>
-    </Provider>
+    <div className='App'>
+      <Count />
+      <Button />
+      <Test />
+    </div>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const logger = ({ getState }) => next => action => {
+  console.log('before dispatch', action);
+  const result = next(action);
+  console.log('after dispatch', getState());
+  return result;
+};
+
+const thunk = ({ getState, dispatch }) => next => action => {
+  if (typeof action === 'function') {
+    return action(dispatch);
+  }
+
+  return next(action);
+};
+
+const reducer = (action, state) => ({ ...state, num: state.num + 1 });
+const preloadedState = { num: 1 };
+const store = configureStore({
+  reducer,
+  middlewares: [thunk, logger],
+  preloadedState,
+});
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
--- src/configure-store.ts
+++ src/configure-store.ts
@@ -2,13 +2,48 @@ export interface ActionCreator<A> {
   (...args: any[]): A;
 }
 
+const applyMiddleware = (middlewares: any) => (configureStore: any) => (
+  ...args: any
+) => {
+  const store = configureStore(...args);
+  let dispatch: any;
+
+  const middlewareAPI = {
+    getState: store.getState,
+    dispatch: (arg: any) => dispatch(arg),
+  };
+
+  const chain: any = middlewares.map((middleware: any) =>
+    middleware(middlewareAPI)
+  );
+  dispatch = compose(chain)(store.dispatch);
+
+  return { ...store, dispatch };
+};
+
+const compose = (fns: any) => {
+  if (fns.length === 0) return (arg: any) => arg;
+  if (fns.length === 1) return fns[0];
+
+  return fns.reduce((a: any, b: any) => (...args: any) => a(b(...args)));
+};
+
 const configureStore = ({
   reducer,
+  middlewares,
   preloadedState,
 }: {
   reducer: any;
+  middlewares: any;
   preloadedState: any;
 }) => {
+  if (middlewares && middlewares.length) {
+    return applyMiddleware(middlewares)(configureStore)({
+      reducer,
+      preloadedState,
+    });
+  }
+
   let currReducer = reducer;
   let currState = preloadedState;
   let listeners: any = [];
@@ -33,6 +68,9 @@ const configureStore = ({
       }
     };
   }
+
+  dispatch({ type: 'init' });
+
   return {
     getState,
     dispatch,
